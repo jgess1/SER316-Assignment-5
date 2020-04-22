fd0ff5614b5b79775cacefef3449d3036d80a546
Jeremy Gess <jjg336@nau.edu>
2020-04-21 22:38:59 -0700

    Wrapper generated files

--- build/scripts/SER316-Assignment-5.bat
+++ build/scripts/SER316-Assignment-5.bat
@@ -17,7 +17,7 @@
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
-@rem  SER316 Assignment 5 Design Patterns startup script for Windows
+@rem  SER316-Assignment-5 startup script for Windows
 @rem
 @rem ##########################################################################
 
@@ -32,7 +32,7 @@ set APP_HOME=%DIRNAME%..
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
-@rem Add default JVM options here. You can also use JAVA_OPTS and SE_R316_ASSIGNMENT_5_DESIGN_PATTERNS_OPTS to pass JVM options to this script.
+@rem Add default JVM options here. You can also use JAVA_OPTS and SE_R316_ASSIGNMENT_5_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS=
 
 @rem Find java.exe
@@ -82,19 +82,19 @@ set CMD_LINE_ARGS=%*
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\lib\SER316 Assignment 5 Design Patterns.jar;%APP_HOME%\lib\cls;%APP_HOME%\lib\junit-4.12.jar;%APP_HOME%\lib\hamcrest-core-1.3.jar
+set CLASSPATH=%APP_HOME%\lib\SER316-Assignment-5.jar;%APP_HOME%\lib\cls;%APP_HOME%\lib\junit-4.12.jar;%APP_HOME%\lib\hamcrest-core-1.3.jar
 
-@rem Execute SER316 Assignment 5 Design Patterns
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %SE_R316_ASSIGNMENT_5_DESIGN_PATTERNS_OPTS%  -classpath "%CLASSPATH%" main.java.Main %CMD_LINE_ARGS%
+@rem Execute SER316-Assignment-5
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %SE_R316_ASSIGNMENT_5_OPTS%  -classpath "%CLASSPATH%" main.java.Main %CMD_LINE_ARGS%
 
 :end
 @rem End local scope for the variables with windows NT shell
 if "%ERRORLEVEL%"=="0" goto mainEnd
 
 :fail
-rem Set variable SE_R316_ASSIGNMENT_5_DESIGN_PATTERNS_EXIT_CONSOLE if you need the _script_ return code instead of
+rem Set variable SE_R316_ASSIGNMENT_5_EXIT_CONSOLE if you need the _script_ return code instead of
 rem the _cmd.exe /c_ return code!
-if  not "" == "%SE_R316_ASSIGNMENT_5_DESIGN_PATTERNS_EXIT_CONSOLE%" exit 1
+if  not "" == "%SE_R316_ASSIGNMENT_5_EXIT_CONSOLE%" exit 1
 exit /b 1
 
 :mainEnd
